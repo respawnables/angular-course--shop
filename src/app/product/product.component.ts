60e2aa3d07d2b86e35ecadd858798d6b9dd77232
Batuhan Özcan <batuhan.ozcan@bil.omu.edu.tr>
2021-07-03 01:55:40 +0300

    build product cards

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,6 +9,12 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
 
+  title = 'Ürün Listesi'
+  products: any[] = [
+    { id: 1, name: 'Laptop', price: 2500, categoryId: 1, description: 'Asus ZenBook', imgLink: "https://www.notebookcheck-tr.com/uploads/tx_nbc2/SL4_AMD_1.jpg" },
+    { id: 2, name: 'Mouse', price: 50, categoryId: 2, description: 'A4Tech', imgLink: 'https://ayb.akinoncdn.com/products/2020/12/02/54024/4b3c9b41-99c8-4783-b29d-4842b809734b_size780x780_quality60_cropCenter.jpg' }
+  ]
+
   ngOnInit(): void {
   }
 
